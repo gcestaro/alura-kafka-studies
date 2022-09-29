44d08b352021cef981cbb84fa390cc3b0507b5e8
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2022-09-29 21:11:05 +0100

    Usage of AUTO_OFFSET_RESET_CONFIG

--- common-kafka/src/main/java/com/github/gcestaro/ecommerce/consumer/KafkaService.java
+++ common-kafka/src/main/java/com/github/gcestaro/ecommerce/consumer/KafkaService.java
@@ -77,6 +77,7 @@ public class KafkaService<T> implements Closeable {
     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup);
     properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
     properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest"); //earliest
 
     properties.putAll(overrideProperties);
 
--- service-email-new-order/src/main/java/com/github/gcestaro/ecommerce/EmailNewOrderService.java
+++ service-email-new-order/src/main/java/com/github/gcestaro/ecommerce/EmailNewOrderService.java
@@ -1,26 +1,23 @@
 package com.github.gcestaro.ecommerce;
 
-import com.github.gcestaro.ecommerce.consumer.KafkaService;
+import com.github.gcestaro.ecommerce.consumer.ConsumerService;
+import com.github.gcestaro.ecommerce.consumer.ServiceRunner;
 import com.github.gcestaro.ecommerce.dispatcher.KafkaDispatcher;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
   private final KafkaDispatcher<Email> emailDispatcher = new KafkaDispatcher<>();
 
-  public static void main(String[] args) throws ExecutionException, InterruptedException {
-    var emailService = new EmailNewOrderService();
-
-    try (var kafkaService = new KafkaService<>(EmailNewOrderService.class.getSimpleName(),
-        "ECOMMERCE_NEW_ORDER", emailService::parse, Map.of())) {
-      kafkaService.run();
-    }
+  public static void main(String[] args) {
+    new ServiceRunner<>(EmailNewOrderService::new).start(1);
   }
 
-  private void parse(ConsumerRecord<String, Message<Order>> record)
+  @Override
+  public void parse(ConsumerRecord<String, Message<Order>> record)
       throws ExecutionException, InterruptedException {
+
     var message = record.value();
     var order = message.getPayload();
 
@@ -35,5 +32,15 @@ public class EmailNewOrderService {
         .continueWith(EmailNewOrderService.class.getSimpleName());
 
     emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(), correlationId, emailCode);
+  }
+
+  @Override
+  public String getTopic() {
+    return "ECOMMERCE_NEW_ORDER";
+  }
+
+  @Override
+  public String getConsumerGroup() {
+    return EmailNewOrderService.class.getSimpleName();
   }
 }
